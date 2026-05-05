445f4f7b491182cb0331c49fba21fc5523d0cd8a
Antonina Dubrovska <antonina.dubrovska@gmail.com>
2026-05-05 18:34:03 +0200

    style: hero page, hero image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
+import heroPage from "../assets/hero-img/hero-page.webp";
+
 const Home = () => {
-  return (
+	return (
 		<div>
-			<h2> Float Into Fun! </h2>
-			<p> Discover bold, funny inflatables made for sunny days. </p>
+			<img src={heroPage} alt="Hero" className="hero-image" />
+			<h2 className="hero-h"> Float Into Fun! </h2>
+			<p className="hero-p"> Discover bold, funny inflatables made for sunny days. </p>
 		</div>
-  );
+	);
 };
 
 export default Home;
