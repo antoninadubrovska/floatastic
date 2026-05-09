b89f00dfdbf3cfbeee5c4fceef73264bee4aa5f5
Antonina Dubrovska <antonina.dubrovska@gmail.com>
2026-05-10 00:00:44 +0200

    feat: preview img addition

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,12 +1,20 @@
 import { Link } from "react-router";
 import { useBuyNow } from "../hooks/useBuyNow";
+import { useProductImage } from "../hooks/useProductImage";
 
 //buy
 const ProductItem = ({ item }) => {
 	const buyNow = useBuyNow();
 
+	const imageUrl = useProductImage(item?.image);
+
 	return (
 		<div className="product-item product-card-preview">
+			<img
+				className="product-card-image-preview"
+				src={imageUrl || item.image}
+				alt={item.name}
+			/>
 			<p className="product-card-preview-name">{item.name}</p>
 			<p className="product-card-preview-price">{item.price} kr</p>
 			<Link to={"/products/" + item.id} className="link-read-more">
